bc4ea2906519ce899e7b9162a5f974a4d599a35e
wayneliu <wliu@allxon.com>
2026-03-20 22:13:36 +0800

    update

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,24 +1,29 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
-local M = {}
-
-M.base46 = {
-	theme = "onedark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+local M = {
+  base46 = {
+    theme = "flexoki",
+  },
+  nvdash = {
+    load_on_startup = true,
+  },
+  cheatsheet = {
+    theme = "simple",
+  },
+  ui = {
+    statusline = {
+      theme = "minimal",
+    },
+  },
+  term = {
+    float = {
+      height = 0.7,
+      width = 0.7,
+    },
+  },
 }
-
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
 
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,17 +12,17 @@ return {
       require "configs.lspconfig"
     end,
   },
-
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "rust",
+      },
+    },
+  },
 }
